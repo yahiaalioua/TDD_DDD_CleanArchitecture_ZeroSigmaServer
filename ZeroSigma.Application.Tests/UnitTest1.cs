f310d2281fb362e9741bcd3ccae0680d58a3f6df
yahiaalioua <yahia@techytypes.com>
2023-04-12 16:58:27 +0200

    initial commit

---
+++ ZeroSigma.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ZeroSigma.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ZeroSigma.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ZeroSigma.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
