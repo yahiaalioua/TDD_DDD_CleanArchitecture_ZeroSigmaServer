ca242520f09b945f5477b34643f5d7e83f51b047
yahiaalioua <yahia@techytypes.com>
2023-04-20 13:45:45 +0200

    [test] ShouldReturnTrueWhenPasswordIsSameAsHash=>pass

--- ZeroSigma.Infrastructure.Tests/Authentication/EncryptionTests.cs
+++ ZeroSigma.Infrastructure.Tests/Authentication/EncryptionTests.cs
@@ -12,15 +12,27 @@ namespace ZeroSigma.Infrastructure.Authentication
         [Fact]
         public void ShouldEncryptPassword()
         {
+            //arrange
             string passwordToEncrypt = "passwordToEncrypt";
             EncryptionService encryptionService = new EncryptionService();
+            //act
             string encryptedPassword=encryptionService.EncryptPassword(passwordToEncrypt);
+            //assert
             Assert.NotEqual(passwordToEncrypt, encryptedPassword);
             Assert.NotNull(encryptionService);
             Assert.True(encryptedPassword.Length < 72);
             Assert.StartsWith("$2a$12$", encryptedPassword);
-
-
+        }
+        [Fact]
+        public void ShouldReturnTrueWhenPasswordIsSameAsHash()
+        {
+            //arrange
+            string passwordToEncrypt = "passwordToEncrypt";
+            EncryptionService encryptionService = new EncryptionService();
+            //act
+            string encryptedPassword = encryptionService.EncryptPassword(passwordToEncrypt);
+            //assert
+            Assert.True(encryptionService.VerifyPassword(passwordToEncrypt,encryptedPassword));
         }
     }
 }
