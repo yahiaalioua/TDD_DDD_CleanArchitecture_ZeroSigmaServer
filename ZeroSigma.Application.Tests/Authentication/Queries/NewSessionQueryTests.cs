e6b0afe0347f68d6c69d73923070a50331476450
yahiaalioua <yahia@techytypes.com>
2023-04-28 10:41:23 +0200

    [test] ShouldReturnTokenNotFoundErrorWhenStoredRefreshTokenIsNull=>Pass

--- ZeroSigma.Application.Tests/Authentication/Queries/NewSessionQueryTests.cs
+++ ZeroSigma.Application.Tests/Authentication/Queries/NewSessionQueryTests.cs
@@ -22,6 +22,9 @@ namespace ZeroSigma.Application.Authentication.Queries
         private readonly Mock<IJwtTokenProcessingService> _jwtTokenProcessingServiceMock;
         private readonly Mock<IUserAccessRepository> _userAccessRepositoryMock;
         private readonly Mock<IAccessTokenProvider> _accessTokenProviderMock;
+        private readonly List<Claim> _claims;
+        private readonly ClaimsIdentity _identity;
+        private readonly ClaimsPrincipal _claimsPrincipal;
         public NewSessionQueryTests()
         {
             _jwtTokenProcessingServiceMock = new Mock<IJwtTokenProcessingService>();
@@ -32,6 +35,15 @@ namespace ZeroSigma.Application.Authentication.Queries
                 _userAccessRepositoryMock.Object,
                 _accessTokenProviderMock.Object
                 );
+            _claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "username"),
+                new Claim(ClaimTypes.NameIdentifier, "userId"),
+                new Claim("name", "John Doe"),
+
+            };
+            _identity=new ClaimsIdentity(_claims, "TestAuthType");
+            _claimsPrincipal = new ClaimsPrincipal(_identity);
         }
 
         [Fact]
@@ -57,5 +69,29 @@ namespace ZeroSigma.Application.Authentication.Queries
             _userAccessRepositoryMock.Verify(x => x.GetUserRefreshToken(It.IsAny<string>()),Times.Never());
             Assert.Equal(result.CustomProblemDetails, NewSessionLogicalValidationErrors.InvalidTokenError);
         }
+        [Fact]
+        public async Task ShouldReturnTokenNotFoundErrorWhenStoredRefreshTokenIsNull()
+        {
+            //arrange
+            var accessToken = "FakeAccessToken836sgsra62,s.nxcbz5a3%4fs";
+            var refreshToken = "FakeRefreshToken836sgsra62,s.nxcbz5a3%4fs";
+            var query = new NewSessionQuery(accessToken, refreshToken);
+            NewSessionRequest newSessionRequest = new(accessToken, refreshToken);
+            var handler = new NewSessionQueryHandler(_sessionProcessingService);
+            _jwtTokenProcessingServiceMock.Setup(x => x.Validate(It.IsAny<string>())).Returns(_claimsPrincipal);
+            _accessTokenProviderMock.Setup(x => x.GenerateAccessToken(It.IsAny<Guid>(), It.IsAny<string>(),
+                It.IsAny<string>())).Returns(It.IsAny<string>());
+            _userAccessRepositoryMock.Setup(x => x.GetUserRefreshToken(It.IsAny<string>())).Returns<UserRefreshToken>(null);
+
+            //act
+            Result<string> result = await handler.Handle(query, default);
+            //assert
+            _jwtTokenProcessingServiceMock.Verify(x => x.Validate(It.IsAny<string>()), Times.Once());
+            _accessTokenProviderMock.Verify(x => x.GenerateAccessToken(It.IsAny<Guid>(), It.IsAny<string>(),
+                It.IsAny<string>()), Times.Never());
+            _userAccessRepositoryMock.Verify(x => x.GetUserRefreshToken(It.IsAny<string>()), Times.Once());
+            Assert.Equal(result.CustomProblemDetails, NewSessionLogicalValidationErrors.TokenNotFoundError);
+        }
     }
+
 }
